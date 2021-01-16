8630bf9fe3a282a7e1f5a86c5ce22e99a7cf20c8
Elena P <elena.pan.ep@gmail.com>
2021-01-16 08:57:33 -0700

    add user template

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const serverPort = process.env.PORT || 5000;
 
 // Routes
-const logs = require("./routes/api/logs");
+const users = require("./routes/api/users");
 
 // Mongo Atlas Config
 const uri = process.env.MONGO_URI;
@@ -30,7 +30,7 @@ app.use(
 app.use(bodyParser.json());
 
 // Routes
-app.use("/api/logs", logs);
+app.use("/api/users", users);
 
 app.listen(serverPort, () => {
   console.log(`Server is running on port ${serverPort}`);
