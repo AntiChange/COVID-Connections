33b05b8647ac9f1d00c4dc5498bf753d463214ff
AntiChange <chr1sf4ng@gmail.com>
2021-01-16 21:02:03 -0700

    Moved Login Screen to one file

--- app/App.js
+++ app/App.js
@@ -1,53 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import {  StyleSheet, Text, View, Dimensions, SafeAreaView, ScrollView, Button, Image } from 'react-native';
 import 'react-native-gesture-handler';
-import PasswordField from './components/PasswordField';
-import UsernameField from './components/UsernameField';
 
+import LoginScreen from "./components/LoginScreen";
 
 
 
 export default function App() {
   return (
-  
-  <SafeAreaView style={styles.container}>
-    <ScrollView style={styles.scrollView}>
-            
-    <View style={styles.test}>
-        
-        <Text
-            style={{fontSize: 30}}
-            >Covid Tracker++</Text>
-          <Text></Text>
-          <UsernameField />
-          <Text></Text>
-          <PasswordField/>
-          <Text></Text>
-
-          <Text 
-          backgroundColor = "#FFFFFFF"
-          style={styles.button1} 
-          //currently placeholder function
-          onPress={async () => {
-            
-          }}
-          > Login </Text>
-
-          <Text></Text>
-
-          <Text 
-          backgroundColor = "#FFFFFFF"
-          style={styles.button2} 
-          //currently placeholder function
-          onPress={async () => {
-            
-          }}
-          > Register </Text>
-        
-      </View>
-            
-    </ScrollView>
-  </SafeAreaView>
+    <View style={styles.container}>
+      <LoginScreen />
+    </View>
   );
 }
 
@@ -57,25 +20,6 @@ export default function App() {
 
 
   const styles = StyleSheet.create({
-    button1: {
-      backgroundColor: '#B19CD9',
-      alignItems: 'center',
-      padding: 10,
-      borderRadius: 16,
-      width: 62,
-      
-      justifyContent: 'center'
-      },
-
-  button2: {
-    backgroundColor: '#B19CD9',
-    alignItems: 'stretch',
-    padding: 10,
-    borderRadius: 16,
-    width: 80,
-    justifyContent: 'center'
-    },
-
   container: {
     flex: 1,
     paddingTop: 100,
@@ -83,21 +27,5 @@ export default function App() {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
-  scrollView: {
-    
-  },
-    
-  text: {
-      fontSize: 50,
-      fontWeight: 'bold'
-  },
-
-  textBox:  {
-    flex: 1,
-  },
- 
-
-  
   });
 
