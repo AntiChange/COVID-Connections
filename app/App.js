d806707d64dfb0f7637458e9c06aef3720c518e7
Elena P <elena.pan.ep@gmail.com>
2021-01-16 20:31:19 -0700

    Move Homescreen to new component

--- app/App.js
+++ app/App.js
@@ -1,115 +1,19 @@
 import React, { Component } from 'react';
 import {
-  Switch,
-  ScrollView,
   StyleSheet,
-  Text,
   View,
-  TouchableOpacity,
 } from 'react-native';
 import Constants from 'expo-constants';
-import * as Animatable from 'react-native-animatable';
-import Collapsible from 'react-native-collapsible';
-import Accordion from 'react-native-collapsible/Accordion';
-import MyComponent from './components/AddConnection';
-
-
-const CONTENT = [
-  {
-    title: 'Bob Joe - Healthy',
-    content: "Status: " + "Self-isolating, " + "Unvaccinated" + "\n\n" + "January 16th" + " - " + "Went to grocery store\nJanuary 14th - Went to friends birthday party\nJanuary 4th - Returned from Italy trip",
-  },
-  {
-    title: 'Billy Bob - Healthy',
-    content: "Status: Frontline Worker, Vaccinated\n\nWeekly - Works as Restaurant waiter\nJanuary 12th - Went to eat in nearby restaurant\nJanuary 1th - Celebrated with 2 friends",
-  },
-  {
-    title: 'Billy Joe - Possible Contact',
-    content: "Status: Sick, Self-isolating\n\nWeekly - Works at office\nJanuary 14th - Started to feel unwell\nJanuary 12th - Went to bar after work",
-  },
-];
-
+import Home from "./components/Home";
 
 
 export default class App extends Component {
-  state = {
-    activeSections: [],
-    collapsed: true,
-    multipleSelect: false,
-  };
-
-  toggleExpanded = () => {
-    this.setState({ collapsed: !this.state.collapsed });
-  };
-
-  setSections = sections => {
-    this.setState({
-      activeSections: sections.includes(undefined) ? [] : sections,
-    });
-  };
-
-  renderHeader = (section, _, isActive) => {
-    return (
-      <Animatable.View
-        duration={400}
-        style={[styles.header, isActive ? styles.active : styles.inactive]}
-        transition="backgroundColor"
-      >
-        <Text style={styles.headerText}>{section.title}</Text>
-      </Animatable.View>
-    );
-  };
-
-  renderContent(section, _, isActive) {
-    return (
-      <Animatable.View
-        duration={400}
-        style={[styles.content, isActive ? styles.active : styles.inactive]}
-        transition="backgroundColor"
-      >
-        <Animatable.Text animation={isActive ? 'zoomIn' : undefined}>
-          {section.content}
-        </Animatable.Text>
-      </Animatable.View>
-    );
-  }
-
   render() {
-    const { multipleSelect, activeSections } = this.state;
-
     return (
       <View style={styles.container}>
-        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
-          <Text style={styles.title}>Connections</Text>
-
-          <MyComponent />
-
-          
-
-          <TouchableOpacity onPress={this.toggleExpanded}>
-            
-          </TouchableOpacity>
-          <Collapsible collapsed={this.state.collapsed} align="center">
-            <View style={styles.content}>
-              <Text>
-                Bacon ipsum dolor amet chuck turducken landjaeger tongue spare
-                ribs
-              </Text>
-            </View>
-          </Collapsible>
-          <Accordion
-            activeSections={activeSections}
-            sections={CONTENT}
-            touchableComponent={TouchableOpacity}
-            expandMultiple={multipleSelect}
-            renderHeader={this.renderHeader}
-            renderContent={this.renderContent}
-            duration={400}
-            onChange={this.setSections}
-          />
-        </ScrollView>
+        <Home />
       </View>
-    );
+    )
   }
 }
 
@@ -118,57 +22,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
     paddingTop: Constants.statusBarHeight,
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: '300',
-    marginBottom: 20,
-  },
-  header: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  headerText: {
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: '500',
-  },
-  content: {
-    padding: 20,
-    backgroundColor: '#fff',
-  },
-  active: {
-    backgroundColor: 'rgba(255,255,255,1)',
-  },
-  inactive: {
-    backgroundColor: 'rgba(245,252,255,1)',
-  },
-  selectors: {
-    marginBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  selector: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  activeSelector: {
-    fontWeight: 'bold',
-  },
-  selectTitle: {
-    fontSize: 14,
-    fontWeight: '500',
-    padding: 10,
-  },
-  multipleToggle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginVertical: 30,
-    alignItems: 'center',
-  },
-  multipleToggle__title: {
-    fontSize: 16,
-    marginRight: 8,
   },
 });
