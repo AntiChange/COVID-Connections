bc270890027305b5ac4ecb209492bcfbbf8515b4
Dhananjay Patki <dpatki64@gmail.com>
2021-01-16 20:16:35 -0700

    made the settings look better. May god have mercy on my soul as i try to do fricking request

--- app/App.js
+++ app/App.js
@@ -1,52 +1,45 @@
 import React, {useEffect, useState } from 'react'
-import { AsyncStorage, StyleSheet, Text, View, Dimensions, SafeAreaView, ScrollView, Button, Image } from 'react-native';
+import { AsyncStorage, ActivityIndicator, StyleSheet, Text, View, Dimensions, SafeAreaView, ScrollView, Button, Image } from 'react-native';
 
 
 
 
 function Toggle({title, id}) {
-  const [text, setText] = useState('Off')
-  settings = getsettings()
-  setText(settings[id])
+  const [text, setText] = useState('0')
+  const [output, setOutput] = useState('Off')
   return (
-    <View style={styles.button}>
-      <View >
-      <Button style={styles.button}
+    <View style={styles.container}>
+      <View style={styles.button}>
+      <Button
+        backgroundColor = {'#fff'}
+        color = {'#00CC20'}
         title={title}
         onPress={() => {
-          if (text == 'On') {
-            setText('Off')
+          if (text == '1') {
+            setText('0')
+            setOutput('Off')
             console.log(`${title}`, false)
           } else {
-            setText('On')
+            setText('1')
+            setOutput('On')
             console.log(`${title}`, true)
           }
           
         }}
       />
-      <Text style={styles.button}>{`${title} is ${text}`}</Text>
+      <Text>{`${title} is ${output}`}</Text>
     </View>
     </View>
   )
 }
 
 export default function App() {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(' /* add in url here*/ https://reactnative.dev/movies.json')
-      .then((response) => response.json())
-      .then((json) => setData(json.settings))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
+  
   return (
     <ScrollView style={styles.scrollView}>
     <View style={styles.container}>
       <Text style={styles.text}>Settings</Text>
-      <Toggle title="Show Status to Close Contacts" id={0} />
+      <Toggle title="Show Status to Close Contacts" id={0}/>
       <Toggle title="Show Status to Other Contacts" id={1} />
       <Toggle title="Show Status to Non-Contacts" id={2} />
       <Toggle title="Hide My Connections" id={3} />
