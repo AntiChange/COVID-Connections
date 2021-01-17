f5e965d33dd79e8e12605c0ac82dad28f4315874
Dhananjay Patki <dpatki64@gmail.com>
2021-01-16 19:18:34 -0700

    helppp i dont know what i'm doing

--- app/App.js
+++ app/App.js
@@ -2,8 +2,12 @@ import React, {useEffect, useState } from 'react'
 import { AsyncStorage, StyleSheet, Text, View, Dimensions, SafeAreaView, ScrollView, Button, Image } from 'react-native';
 
 
-function Toggle({title}) {
+
+
+function Toggle({title, id}) {
   const [text, setText] = useState('Off')
+  settings = getsettings()
+  setText(settings[id])
   return (
     <View style={styles.button}>
       <View >
@@ -27,28 +31,26 @@ function Toggle({title}) {
 }
 
 export default function App() {
-  const getAllLogs = async () => {
-    const url = logURL;
-    try {
-        return fetch(url, { method: "GET", })
-            .then((response) => response.json())
-            .then((logs) => {
-                return logs;
-            })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(' /* add in url here*/ https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => setData(json.settings))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <ScrollView style={styles.scrollView}>
     <View style={styles.container}>
       <Text style={styles.text}>Settings</Text>
-      <Toggle title="Show Status to Close Contacts" />
-      <Toggle title="Show Status to Other Contacts"/>
-      <Toggle title="Show Status to Non-Contacts"/>
-      <Toggle title="Hide My Connections"/>
-      <Toggle title="Hide Me From Other's Connections List"/>
+      <Toggle title="Show Status to Close Contacts" id={0} />
+      <Toggle title="Show Status to Other Contacts" id={1} />
+      <Toggle title="Show Status to Non-Contacts" id={2} />
+      <Toggle title="Hide My Connections" id={3} />
+      <Toggle title="Hide Me From Other's Connections List" id={4} />
     </View>
     </ScrollView>
   )
